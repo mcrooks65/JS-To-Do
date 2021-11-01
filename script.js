4cde6da7902965ab6c1a644b6271dd166e7a69d7
mcrooks65 <mfcrooks@gmail.com>
2021-11-01 14:30:22 -0700

    Add delete button functionality

--- script.js
+++ script.js
@@ -39,3 +39,11 @@ enterButton.addEventListener("click", (e) => {
     });
     ul.innerHTML = htmlCode;
    }
+
+   function deleteTodo(ind) {
+    let todo = localStorage.getItem("todo");
+    todoArray = JSON.parse(todo);
+    todoArray.splice(ind, 1);
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
+   }
